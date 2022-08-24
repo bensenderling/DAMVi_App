811a4ee1c7d1e88d65df0abd5f00e6f63abf861f
Ben Senderling <bensenderling@gmail.com>
2022-08-24 17:10:24 -0400

    Made very minor changes.
    I started looking at if h5 files from Pfizer could be loaded into the app. However the files of interest do not contain the measures I was looking for.

---
+++ Load/load_h5_Pfizer.m
@@ -0,0 +1,51 @@
+function h5_Pfizer = load_h5_Pfizer(file)
+
+dbstop if error
+
+h5_APDM.meta = h5info(file);
+
+%%
+
+for i = 1:length({h5_APDM.meta.Groups(2).Groups.Name})
+    name = h5readatt(file,[h5_APDM.meta.Groups(2).Groups(i).Name '/Configuration'], 'Label 0');
+    name = regexprep(name, {' ', char(0)}, {'_', ''});
+    time = double(h5read(file,[h5_APDM.meta.Groups(2).Groups(i).Name '/Time']));
+    h5_APDM.(name).freq = 1/mean(diff(time - time(1))/1e6);
+    h5_APDM.(name).data.acc = h5read(file, [h5_APDM.meta.Groups(2).Groups(i).Name '/Accelerometer'])';
+    h5_APDM.(name).data.mag = h5read(file, [h5_APDM.meta.Groups(2).Groups(i).Name '/Magnetometer'])';
+    h5_APDM.(name).data.gyr = h5read(file, [h5_APDM.meta.Groups(2).Groups(i).Name '/Gyroscope'])';
+end
+% 
+% figure
+% subplot(2,3,4),plot(h5_APDM.Left_Foot(:,1),h5_APDM.Left_Foot(:,2),h5_APDM.Right_Foot(:,1),h5_APDM.Right_Foot(:,2))
+% xlabel('Time (s)')
+% ylabel('x(Time)')
+% axis tight
+% subplot(2,3,5),plot(h5_APDM.Left_Foot(:,1),h5_APDM.Left_Foot(:,3),h5_APDM.Right_Foot(:,1),h5_APDM.Right_Foot(:,3))
+% xlabel('Time (s)')
+% ylabel('y(Time)')
+% axis tight
+% subplot(2,3,6),plot(h5_APDM.Left_Foot(:,1),h5_APDM.Left_Foot(:,4),h5_APDM.Right_Foot(:,1),h5_APDM.Right_Foot(:,4))
+% xlabel('Time (s)')
+% ylabel('z(Time)')
+% axis tight
+% legend('Left Foot', 'Right Foot')
+% 
+% subplot(2,3,1),plot(h5_APDM.Lumbar(:,1),h5_APDM.Lumbar(:,2))
+% xlabel('Time (s)')
+% ylabel('x(Time)')
+% axis tight
+% subplot(2,3,2),plot(h5_APDM.Lumbar(:,1),h5_APDM.Lumbar(:,3))
+% xlabel('Time (s)')
+% ylabel('y(Time)')
+% axis tight
+% subplot(2,3,3),plot(h5_APDM.Lumbar(:,1),h5_APDM.Lumbar(:,4))
+% xlabel('Time (s)')
+% ylabel('y(Time)')
+% axis tight
+% 
+
+
+
+
+
