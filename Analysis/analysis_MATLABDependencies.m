1bc05119fc87430f2935ff87aac252fb123c4239
Ben Senderling <bensenderling@gmail.com>
2022-12-14 15:09:46 -0500

    Added an export option.
    Added an option to export tabular results from the results data types. Finished writing an analysis script to determine code dependencies. Added information on them to the user manual.

---
+++ Analysis/analysis_MATLABDependencies.m
@@ -0,0 +1,39 @@
+function app = analysis_MATLABDependencies(app, data, analysis)
+% app = analysis_MATLABDependencies(app, data)
+% inputs  - app, required mlapp object.
+%         - data, BAR App data structure.
+%         - analysis, string of the analysis type.
+% Remarks
+% - This analysis script find the dependencies of all the found m-files. It does not produce any metrics but is meant to provide information on what
+%   MATLAB products are needed for the BAR App and it's methods.
+% Future Work
+% - None.
+% Nov 2022 - Created by Ben Senderling, bsender@bu.edu
+%% Begin Code
+
+% Get the file names.
+files = fieldnames(data.raw);
+
+% Start iterating through the file names.
+for i = 1:length(files)
+
+    % Identify the required functions and programs.
+    [fList, pList] = matlab.codetools.requiredFilesAndProducts(data.raw.(files{i}).file.data.file);
+
+    if i == 1
+        pListAll = struct2table(pList);
+        fListAll = fList';
+    else
+        pListAll = [pListAll; struct2table(pList)];
+        fListAll = [fListAll; fList'];
+    end
+
+end
+
+data.res.file.file.data.fListAll = unique(fListAll);
+data.res.file.file.data.pListAll = unique(pListAll);
+
+% Run the public BAR App analysisComplete method to get the data back into the app.
+analysisComplete(app, data, analysis)
+
+end
---
+++ Load/load_m_MATLAB.m
@@ -0,0 +1,14 @@
+function m_MATLAB = load_m_MATLAB(file)
+% m_MATLAB = load_m_MATLAB(file)
+% inputs  - file, this is the file path to the file to load.
+% outputs - dataout, formated data for the BAR app.
+% Remarks
+% - This function is a dummy load function that allows m-files to be found by the BAR App.
+% Future Work
+% - None.
+% Dec 2022 - Created by Ben Senderling, bsender@bu.edu
+%% Begin Code
+
+m_MATLAB.file.data.file = file;
+
+end
