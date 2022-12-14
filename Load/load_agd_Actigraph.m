1234936a1924c229e4133da3825f8da071c1c418
Ben Senderling <bensenderling@gmail.com>
2022-12-14 17:18:00 -0500

    Added information to user manual.
    Added some load functions after adding their descriptions to Appendix A in the user manual.

---
+++ Load/load_agd_Actigraph.m
@@ -0,0 +1,16 @@
+function dataout = load_agd_Actigraph(file)
+% [dataout] = load_agd_Actigraph(file)
+% inputs  - file, this input is not used.
+% outputs - dataout, this output is not used
+% Remarks
+% - This function is a placeholder so agd files can be moved and copied
+%   within the BAR App. These files are binary and currently there is not a
+%   way to load them directly into MATLAB.
+% Future Work
+% - Possibly these files could be read directly into MATLAB.
+% Aug 2022 - Created by Ben Senderling, bsender@bu.edu
+%% Begin Code
+
+dataout = [];
+
+end
