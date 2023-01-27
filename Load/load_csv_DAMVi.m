677ea0f52a30e5139bdac44ef114774139a08966
Ben Senderling <bensenderling@gmail.com>
2023-01-27 12:46:32 -0500

    Fixed bugs in the DAMVi csv import and expanded functionality of Grouping Module.
    Added code to load_csv_DAMVi to correctly load prior data exports with variable dimension and number information.
    Added a switch to the Grouping Module that will average data within a signal before merging.

--- Load/load_csv_DAMVi.m
+++ Load/load_csv_DAMVi.m
@@ -11,7 +11,8 @@ function csv_BAR = load_csv_DAMVi(file)
 %   need to be modified first before the change could be made here.
 % Nov 2022 - Created by Ben Senderling, bsender@bu.edu
 % Jan 2023 - Modified by Ben Senderling, bsender@bu.edu
-%          - Changed the use of 'measure' to 'object'. Added code to separate groups for files and objects.
+%          - Changed the use of 'measure' to 'object'. Added code to separate file and object groups. Added to code to fix issue with multiple
+%            dimensions and numbers not importing.
 
 % The data can be read as a table with the variable names.
 data = readtable(file, 'VariableNamingRule', 'preserve');
@@ -25,7 +26,7 @@ fields(strcmp(fields, 'Properties') | strcmp(fields, 'Row') | strcmp(fields, 'Va
 indGroup = find(contains(fields, 'group'));
 indGroupFile = indGroup(indGroup < find(contains(fields, 'object')));
 % Find the indexes of the signals by removing the options.
-indSignal = find(~contains(fields, 'group') & ~contains(fields, 'file') & ~contains(fields, 'object'));
+indSignal = find(~contains(fields, 'group') & ~contains(fields, 'file') & ~contains(fields, 'object') & ~contains(fields, 'dimension') & ~contains(fields, 'number'));
 % The group columns that occur after the object but before the signals are assigned to the object level.
 indGroupObject = indGroup(indGroup > find(contains(fields, 'object')) & indGroup < min(indSignal));
 
@@ -50,56 +51,38 @@ for i = 1:height(data)
         % be re-imported. This is the opposite of the export means option
         % in the export tab.
         if isfield(csv_BAR.raw.(data.file{i}), data.object{i}) && isfield(csv_BAR.raw.(data.file{i}).(data.object{i}), 'data') && isfield(csv_BAR.raw.(data.file{i}).(data.object{i}).data, fields{indSignal(j)})
-            if isfield(data, 'dimension')
-                d = data.dimension{i};
+            % Check if there is a dimension column. If there is use the value, otherwise set it to 1.
+            if any(strcmp(fields, 'dimension'))
+                d = data.dimension(i);
             else
                 d = 1;
             end
-            if isfield(data, 'number')
-                n = data.number{i};
+            % Check if there is a number column. If there is use the value, otherwise set it to 1.
+            if any(strcmp(fields, 'number'))
+                n = data.number(i);
             else
                 n = 1;
 %                 n = length(csv_BAR.raw.(data.file{i}).(data.object{i}).data.(fields{indSignal(j)})) + 1;
             end
+            % Save the data into the structure using all the fields from the csv file to reconstruct the DAMVi data structure.
             csv_BAR.raw.(data.file{i}).(data.object{i}).data.(fields{indSignal(j)})(n, d) = data.(fields{indSignal(j)})(i);
         else
+            % Check if there is a dimension column. If there is use the value, otherwise set it to 1.
             if isfield(data, 'dimension')
-                d = data.dimension{i};
+                d = data.dimension(i);
             else
                 d = 1;
             end
+            % Check if there is a number column. If there is use the value, otherwise set it to 1.
             if isfield(data, 'number')
-                n = data.number{i};
+                n = data.number(i);
             else
                 n = 1;
             end
+            % Save the data into the structure using all the fields from the csv file to reconstruct the DAMVi data structure.
             csv_BAR.raw.(data.file{i}).(data.object{i}).data.(fields{indSignal(j)})(n, d) = data.(fields{indSignal(j)})(i);
         end
     end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 end
